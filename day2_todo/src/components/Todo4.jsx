eca3e7ddf2c32b706457c14fb10927085e94e7ad
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-05-19 00:11:22 +0530

    comit

--- day2_todo/src/components/Todo4.jsx
+++ day2_todo/src/components/Todo4.jsx
@@ -6,7 +6,7 @@ const[xxx,setXxx]=useState([]);
   return (
     <div>Todo4
         <input  value={yyy} onChange={(e)=>{setYyy(e.target.value)}}/>
-        <button onClick={()=>{setXxx([...xxx,{id:1,value:yyy}])
+        <button onClick={()=>{setXxx([...xxx,{id:Date.now(),value:yyy}])
         setYyy("")}}>Add</button>
        { xxx.map((e)=>(<div key={e.id}>{e.value}</div>))}
     </div>
