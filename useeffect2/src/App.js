7734467664d9516d0ff60c9cda9bf7e38f51f5f6
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-05-29 21:05:50 +0530

    commit

--- useeffect2/src/App.js
+++ useeffect2/src/App.js
@@ -3,26 +3,39 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const[page,setPage] = useState(1);
+  const [page, setPage] = useState(1);
+  const [limit ,setLimit]= useState(5);
   const [todos, setTodo] = useState([]);
+  const [totalCount, setTotalcount] = useState(0);
   useEffect(() => {
-    axios
-    .get(`http://localhost:8080/todo?_page=${page}&_limit=5`)
-    .then((r)=>{
+    axios.get(`http://localhost:8080/todo?_page=${page}&_limit=${limit}`).then((r) => {
       setTodo(r.data);
+      setTotalcount(Number(r.headers["x-total-count"]));
     });
-  
-  }, [page]);
+  }, [page,limit]);
   return (
     <div className="App">
+      <button
+        disabled={totalCount < page * limit}
+        onClick={() => setPage(page + 1)}
+      >
+        Next
+      </button>
+      <select onChange={(e)=>setLimit(Number(e.target.value))}>
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="30">30</option>
+      </select>
+      <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
+        Prev
+      </button>
+      
       {todos.map((todo, index) => (
         <div key={todo.id}>
           {todo.id} . {todo.value}
         </div>
       ))}
-      <br />
-      <button onClick={() => setPage(page + 1)}>Next</button>
-      <button onClick={() => setPage(page - 1)}>Prev</button>
     </div>
   );
 }
