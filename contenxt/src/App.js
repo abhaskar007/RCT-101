182a8fff07186ab8f82198db11e722c52bc58f84
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-06-02 02:33:43 +0530

    useReducer

--- contenxt/src/App.js
+++ contenxt/src/App.js
@@ -1,16 +1,37 @@
-import React, { useState }  from 'react';
+import React, { useReducer, useState }  from 'react';
 import './App.css';
 
+function reducer(state,action)
+{
+  switch(action.type){
+    case 'inc':
+    return state+1;
+
+    case 'dec':
+      return state-1;
+
+      default:
+      return  state;
+  }
+
+}
 
 function App() {
-  const [counter,setCounter] = useState({value:""});
-  const handleOnChange = (e) => {
-    setCounter({value:e.target.value})
-  }
+  // const [counter,setCounter] = useState(0);
+
+  const [state,dispatch] = useReducer(reducer,0);
+
+
+  // const handleOnChange = (e) => {
+  //   setCounter({value:e.target.value})
+  // }
   return (
     <div className="App">
-     <input type="text" value={counter.value} onChange={handleOnChange}/>
-  
+     Counter:{state}
+     <div>
+     <button onClick={()=>dispatch({type:'inc'})}>+</button>
+     <button onClick={()=>dispatch({type:'dec'})}>-</button>
+     </div>
     </div>
   );
 }
