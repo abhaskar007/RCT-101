2f407d9974d20ed012abb5ef01d2f9b4cf88533c
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-06-05 23:18:26 +0530

    comit

--- routing1/src/componenets/Navbar.jsx
+++ routing1/src/componenets/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
       <Link to="/products">Products</Link>
 
       <button onClick={() => handleOnClick(1)}>Go To Product 1</button>
-      <button onClick={() => handleOnClick(2)}>Go To Product 2</button>
+      <button onClick={() => handleOnClick(2)}>Go To Product 2 </button>
     </div>
   );
 };
