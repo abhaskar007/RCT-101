8ebad0aa9a3b157379dd9ec9623dce3293a539c0
abhaskar007 <abhaskar.bhaskar@gmail.com>
2022-06-13 16:55:40 +0530

    comit

--- react_intro_albert/src/componets/Gallery.jsx
+++ react_intro_albert/src/componets/Gallery.jsx
@@ -2,24 +2,41 @@ import React from 'react'
 import { Avatar } from './Avatar'
 import { Button } from './Button'
 
+const person = {
+    id:1,
+    image: "https://reqres.in/img/faces/1-image.jpg",
+    name: "Abhishek",
+    text : "click here to know me"
+}
+const person2 = {
+    id:2,
+    image: "https://reqres.in/img/faces/6-image.jpg",
+    name: "Tracey",
+    text : "To keep ReqRes free"
+}
+const person3 = {
+    "id":3,"email":"emma.wong@reqres.in","name":"Emma","last_name":"Wong","image":"https://reqres.in/img/faces/3-image.jpg"
+}
+
 export const Gallery = () => {
-    const person = {
-        image: "https://reqres.in/img/faces/1-image.jpg",
-        name: "Abhishek",
-        text : "click here to know me"
-    }
-    const person2 = {
-        image: "https://reqres.in/img/faces/6-image.jpg",
-        name: "Tracey",
-        text : "To keep ReqRes free"
-    }
+    const personArray = [person,person2,person3];
+
+const Result = personArray.map((items)=>
+{
+    return (
+<div key={items.id}>
+    <Avatar person={items} size={100}/>
+    <Button text = {items.text} />
+</div>
+    );
+});
+
+
+   
   return (
     <div>
+{Result}
 
-<Avatar person={person} size = {100} />
-<Button text={person.text} />
-<Avatar person={person2} size = {100} />
-<Button text={person2.text} />
     </div>
   )
 }
